8b0bab8a0b08a7df1166e408a269521b088cf5b6
Carlos Urquiza and Gray Hildreth <webquiza@gmail.com>
2016-05-25 20:17:58 -0700

    This commit will display radio selection.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,15 @@
      var InputFood = $("input#InputFood").val();
      var InputMusic = $("input#InputMusic").val();
      var InputMovie = $("input#InputMovie").val();
+     var color = $( "input:radio[name=color]:checked" ).val();
 
 
      $(".InputName").text(InputName);
      $(".InputFood").text(InputFood);
      $(".InputMusic").text(InputMusic);
      $(".InputMovie").text(InputMovie);
+     $(".color").text(color);
+
      $("#answers").show();
     event.preventDefault();
    });
